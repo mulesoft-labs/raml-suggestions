4c802604c3349b8d8cffedf74717e3cf0dd32f5c
dreamflyer <gleb@onpositive.com>
2016-08-31 21:09:23 +0600

    adding tests.

--- src/test/completionTestApi.ts
+++ src/test/completionTestApi.ts
@@ -6,7 +6,7 @@ import path = require('path');
 import fs = require('fs');
 import {CompletionProvider} from "../index";
 
-class ContentProvider implements completion.ICompletionContentProvider {
+class SyncContentProvider implements completion.ICompletionContentProvider {
     contentDirName(content: completion.IContent): string {
         var contentPath = content.getPath();
         
@@ -33,6 +33,18 @@ class ContentProvider implements completion.ICompletionContentProvider {
 
     readDir(dirPath: string): string[] {
         return fs.readdirSync(dirPath);
+    }
+    
+    existsAsync(path: string): Promise<boolean> {
+        return Promise.reject("method disabled");
+    }
+    
+    readDirAsync(path: string): Promise<string[]> {
+        return Promise.reject("method disabled");
+    }
+    
+    isDirectoryAsync(path: string): Promise<boolean> {
+        return Promise.reject("method disabled");
     }
 }
 
@@ -75,7 +87,7 @@ function resolve(testPath: string): string {
 }
 
 export function completionByOffset(filePath: string, offset: number): string {
-    var completionProvider: CompletionProvider = new CompletionProvider(new ContentProvider());
+    var completionProvider: CompletionProvider = new CompletionProvider(new SyncContentProvider());
 
     var content: completion.IContent = new FSContent(resolve(filePath));
     var position: completion.IPosition = new Position(offset);
@@ -85,8 +97,19 @@ export function completionByOffset(filePath: string, offset: number): string {
     return result.map((suggestion: any) => suggestion.displayText || suggestion.text).join(', ');
 }
 
+export function completionByOffsetAsync(filePath: string, offset: number, callback: (result: string) => void): void {
+    var completionProvider: CompletionProvider = new CompletionProvider(completion.getContentProvider(new AsyncFSResolver()));
+
+    var content: completion.IContent = new FSContent(resolve(filePath));
+    var position: completion.IPosition = new Position(offset);
+
+    completionProvider.suggestAsync(new completion.CompletionRequest(content, position), true).then(result => {
+        callback(result.map((suggestion: any) => suggestion.displayText || suggestion.text).join(', '));
+    });
+}
+
 export function completionByUniqueEntry(filePath: string, entry: string, begin: boolean = false): string {
-    var completionProvider: CompletionProvider = new CompletionProvider(new ContentProvider());
+    var completionProvider: CompletionProvider = new CompletionProvider(new SyncContentProvider());
 
     var content: completion.IContent = new FSContent(resolve(filePath));
 
@@ -95,4 +118,84 @@ export function completionByUniqueEntry(filePath: string, entry: string, begin: 
     var result = completionProvider.suggest(new completion.CompletionRequest(content, position), true);
 
     return result.map((suggestion: any) => suggestion.displayText || suggestion.text).join(', ');
+}
+
+export function completionByUniqueEntryAsync(filePath: string, entry: string, begin: boolean = false, callback: (result: string) => void): void {
+    var completionProvider: CompletionProvider = new CompletionProvider(completion.getContentProvider(new AsyncFSResolver()));
+
+    var content: completion.IContent = new FSContent(resolve(filePath));
+
+    var position: completion.IPosition = new Position(begin ? (content.getText().indexOf(entry)) : offsetForEntry(entry, content.getText()));
+
+    completionProvider.suggestAsync(new completion.CompletionRequest(content, position), true).then(result => {
+        callback(result.map((suggestion: any) => suggestion.displayText || suggestion.text).join(', '));
+    });
+}
+
+class AsyncFSResolver {
+    content(path:string): string{
+        return null;
+    }
+
+    list(path: string): string[] {
+        return null;
+    };
+
+    exists(path: string): boolean {
+        return false;
+    }
+
+    contentAsync(path:string):Promise<string> {
+        return new Promise((resolve: any, reject: any) => {
+            fs.readFile(path, (err: any, content: any) => {
+                if(err) {
+                    reject(err);
+
+                    return;
+                }
+
+                resolve(content.toString());
+            })
+        });
+    }
+
+    dirname(filePath: string): string {
+        return path.dirname(filePath);
+    }
+
+    resolve(contextPath: string, relativePath: string): string {
+        return path.resolve(contextPath, relativePath);
+    }
+
+    extname(filePath: string): string {
+        return path.extname(filePath);
+    }
+
+    isDirectory(filePath: string): boolean {
+        return fs.statSync(filePath).isDirectory();
+    }
+
+    existsAsync(path: string): Promise<boolean> {
+        return new Promise(resolve => {
+            fs.exists(path, (result) => {
+                resolve(result);
+            })
+        });
+    }
+
+    listAsync(path: string): Promise<string[]> {
+        return new Promise(resolve => {
+            fs.readdir(path, (err, result) => {
+                resolve(result);
+            })
+        });
+    }
+
+    isDirectoryAsync(path: string): Promise<boolean> {
+        return new Promise(resolve => {
+            fs.stat(path, (err, stats) => {
+                resolve(stats.isDirectory());
+            })
+        });
+    }
 }
