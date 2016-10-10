4abfeaa1c940bf67103b57c71ae651b78d1ad3aa
dreamflyer <gleb@onpositive.com>
2016-10-11 04:02:06 +0600

    values completion fix. closes #14, closes mulesoft/api-workbench#328.

--- src/completionProvider.ts
+++ src/completionProvider.ts
@@ -181,6 +181,9 @@ function getSuggestions(request: CompletionRequest, provider: CompletionProvider
                         preParsedAst: parserApi.hl.IParseResult = undefined): Suggestion[] {
     provider.currentRequest = request;
 
+    var line = getLine(request);
+    var pr = getPrefix(request);
+
     try {
         if(provider.level > 100){
             return;
@@ -1213,11 +1216,75 @@ function propertyCompletion(node: parserApi.hl.IHighLevelNode, request: Completi
     return rs;
 }
 
-export function valueCompletion(node: parserApi.hl.IParseResult, attr: parserApi.hl.IAttribute, request: CompletionRequest, provider: CompletionProvider){
+function isUnexspected(symbol: string): boolean {
+    if(symbol === "'") {
+        return true;
+    }
+
+    if(symbol === '"') {
+        return true;
+    }
+    
+    return false;
+}
+
+function isValueBroken(request: CompletionRequest) {
+    var text = request.content.getText();
+    var offset = request.content.getOffset();
+    var prefix = request.prefix();
+
+    var beginning = text.substring(0, offset);
+    
+    var value = beginning.substring(beginning.lastIndexOf(':') + 1).trim();
+    
+    if(!value.length) {
+        return false;
+    }
+    
+    if(value[value.length - 1] === ',') {
+        if(value.indexOf('[') < 0) {
+            return true;
+        }
+    }
+    
+    if(beginning[beginning.length - 1] === ' ') {
+        if(/^\w$/.test(value[value.length - 1])) {
+            return true;
+        } else if(value[value.length - 1] === ',') {
+            if(value.indexOf('[') < 0) {
+                return true;
+            }
+        }
+    }
+    
+    if(/^\w+$/.test(prefix)) {
+        value = value.substring(0, value.lastIndexOf(prefix)).trim();
+
+        if(/^\w$/.test(value[value.length - 1])) {
+            return true;
+        } else if(value[value.length - 1] === ',') {
+            if(value.indexOf('[') < 0) {
+                return true;
+            }
+        }
+    }
+    
+    if(isUnexspected(value[value.length - 1])) {
+        return true;
+    }
+
+    return false;
+}
+
+export function valueCompletion(node: parserApi.hl.IParseResult, attr: parserApi.hl.IAttribute, request: CompletionRequest, provider: CompletionProvider) {
     var hlnode = <parserApi.hl.IHighLevelNode>node;
 
     var text = request.content.getText();
     var offset = request.content.getOffset();
+
+    if(isValueBroken(request)) {
+        return [];
+    }
 
     if(attr) {
         var p: parserApi.hl.IProperty = attr.property();
@@ -1329,6 +1396,8 @@ function findASTNodeByOffset(ast : parserApi.hl.IHighLevelNode, request: Complet
 function enumValues(property: parserApi.ds.Property, parentNode: parserApi.hl.IHighLevelNode): Suggestion[] {
     if(parentNode) {
         if(property.getAdapter(parserApi.ds.RAMLPropertyService).isTypeExpr()) {
+
+
             var associatedType = parentNode.associatedType();
 
             var parentDefinition = parentNode.definition();
